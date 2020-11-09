d579b3adc14cde4217f6b2500ee07a3a13c7c738
travis <travis.bale@gmail.com>
2020-11-08 20:32:47 -0600

    Check status code before committing db changes

--- scorecard/models/__init__.py
+++ scorecard/models/__init__.py
@@ -10,5 +10,8 @@ bp = Blueprint("database", __name__)
 @bp.after_app_request
 def commit_session(response):
     """After each successful request commit the current database session."""
-    db.session.commit()
+    if response.status_code < 400:
+        # TODO: Might have to catch exceptions here
+        db.session.commit()
+
     return response
