565ea4e1fc5ebf4898aca2d37ca58dc6f6a89305
travis <travis.bale@gmail.com>
2020-11-23 19:19:36 -0600

    Don't run the migrations on the test container
    All the tables in the test database are created and destroyed each time
    the tests are run.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-if [ $1 = 'development' ]
+if [ $1 = 'development' ] || [ $1 = 'test' ]
 then
     # Install requirements
     pip install --upgrade pip
     pip install -r requirements/production.txt
 fi
 
-# Apply database schema migrations
-flask db upgrade
+if [ $1 != 'test' ]
+then
+    # Apply database schema migrations
+    flask db upgrade
+fi
 
-if [ $1 = 'development' ]
+if [ $1 = 'development' ] || [ $1 = 'test' ]
 then
     # Run the service
     flask run -h 0.0.0.0
