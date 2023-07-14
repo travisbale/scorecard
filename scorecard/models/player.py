ba7a7996416e11cffbb10a4a054d6c2201748d71
Travis Bale <travis.bale@gmail.com>
2023-07-14 14:49:48 -0500

    Add email to the minimal player attributes

--- scorecard/models/player.py
+++ scorecard/models/player.py
@@ -96,6 +96,7 @@ class Player(BaseModel):
 
 class MinimalPlayerSchema(BaseSchema):
     id = fields.Integer(dump_only=True)
+    email = fields.Email(dump_only=True)
     first_name = fields.String(required=True)
     last_name = fields.String(required=True)
     full_name = fields.String(dump_only=True)
